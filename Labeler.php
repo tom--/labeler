701bf33d950dd7572448f56802757c43c0c4a57f
Tom Worster <fsb@thefsb.org>
2015-12-28 21:37:28 -0500

    init comit

---
+++ Labeler.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace spinitron\labeler;
+
+use Guzzle\Http\Client;
+use MusicBrainz\Filters\RecordingFilter;
+use MusicBrainz\HttpAdapters\GuzzleHttpAdapter;
+use MusicBrainz\MusicBrainz;
+use MusicBrainz\Recording;
+
+class Labeler
+{
+    public $minScore = 60;
+
+    protected $brainz;
+
+    public function __construct()
+    {
+        $this->brainz = new MusicBrainz(new GuzzleHttpAdapter(new Client()));
+        $this->brainz->setUserAgent('Spinitron Labeler', '0.1', 'http://spinitron.bitbucket.com/labeler');
+    }
+
+    public static function logerr($error)
+    {
+        fwrite(STDERR, $error);
+    }
+
+    public static function prepare($string)
+    {
+        return trim(preg_replace('%[\pZ\p{Cc}]+%u', ' ', $string), ' \'"');
+    }
+
+    function cleanup($artist, $song, $album)
+    {
+        $answer = [static::prepare($artist), static::prepare($song), static::prepare($album), ''];
+
+        try {
+            $result = $this->brainz->search(new RecordingFilter(
+                ["artist" => $artist, "recording" => $song, "release" => $album]
+            ), 1);
+
+            if (isset($result[0]->score) && $result[0]->score >= $this->minScore) {
+                /** @var Recording $mbRecording */
+                $mbRecording = $result[0];
+                $answer[1] = $result[0]->title;
+
+                $mbArtist = $this->brainz->lookup('artist', $mbRecording->artistID);
+                if (isset($mbArtist['name'])) {
+                    $answer[0] = $mbArtist['name'];
+                }
+
+                $mbRelease = $this->brainz->lookup('release', $mbRecording->releases[0]->id, ['labels']);
+                if (isset($mbRelease['title'])) {
+                    $answer[2] = $mbRelease['title'];
+                }
+                if (isset($mbRelease['label-info'][0]['label']['name'])) {
+                    $answer[3] = $mbRelease['label-info'][0]['label']['name'];
+                }
+            }
+        } catch (\Exception $e) {
+            //static::logerr($e->getMessage());
+        }
+
+        return $answer;
+    }
+}
---
+++ labeler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace spinitron\labeler;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$delimiter = ',';
+$input = STDIN;
+$output = STDOUT;
+$script = array_shift($argv);
+
+$usage = <<<STRING
+$script [-t] [in [out]]"
+  -t   use tab separators rather than commas
+  in   input file name, or - for for stdin
+  out  output file name, or - for for stdout
+
+STRING;
+
+$arg = array_shift($argv);
+if ($arg === '-t') {
+    $delimiter = "\t";
+    $arg = array_shift($argv);
+} elseif ($arg && $arg[0] === '-') {
+    if ($arg !== '-h' && $arg !== '--help'){
+        Labeler::logerr("invalid option: $arg\n");
+    }
+    Labeler::logerr($usage);
+    exit(1);
+}
+if ($arg) {
+    if ($arg !=='-') {
+        $input = fopen($arg, 'r');
+        if ($input === false) {
+            Labeler::logerr("cannot open input file: $arg\n" . $usage);
+            exit(1);
+        }
+    }
+    $arg = array_shift($argv);
+}
+if ($arg) {
+    if ($arg !=='-') {
+        $output = fopen($arg, 'r');
+        if ($output === false) {
+            Labeler::logerr("cannot open input file: $arg\n" . $usage);
+            exit(1);
+        }
+    }
+}
+
+$labeler = new Labeler();
+
+while (!feof($input)) {
+    $item = fgetcsv($input, 0, $delimiter);
+    if ($item === false) {
+        break;
+    }
+
+    if (count($item) === 2) {
+        $item[] = '';
+    }
+
+    if (count($item) > 2) {
+        $cleaned = $labeler->cleanup($item[0], $item[1], $item[2]);
+        fputcsv($output, $cleaned, $delimiter);
+    }
+}
